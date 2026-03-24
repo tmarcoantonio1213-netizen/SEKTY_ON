0d285d251eea670b2c9ac5bbde692f5fba055bf0
4nt-M4r206 <4nt-M4r206@4NT-M4R206>
2026-03-24 17:20:35 -0600

    Agregar archivos de proyecto.

---
+++ SEKTY_ON/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SEKTY_ON
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var laboratorios = new List<object>
+            {
+                new { Nombre = "Laboratorio de Redes", Ubicacion = "Edificio A" },
+                new { Nombre = "Laboratorio de Cómputo 1", Ubicacion = "Edificio B" },
+                new { Nombre = "Sala de Innovación Digital", Ubicacion = "Edificio C" },
+                new { Nombre = "Laboratorio de Redes", Ubicacion = "Edificio A" },
+                new { Nombre = "Laboratorio de Cómputo 1", Ubicacion = "Edificio B" },
+                new { Nombre = "Sala de Innovación Digital", Ubicacion = "Edificio C" },
+                new { Nombre = "Laboratorio de Redes", Ubicacion = "Edificio A" },
+                new { Nombre = "Laboratorio de Cómputo 1", Ubicacion = "Edificio B" },
+                new { Nombre = "Sala de Innovación Digital", Ubicacion = "Edificio C" },
+                new { Nombre = "Laboratorio de Redes", Ubicacion = "Edificio A" },
+                new { Nombre = "Laboratorio de Cómputo 1", Ubicacion = "Edificio B" },
+                new { Nombre = "Sala de Innovación Digital", Ubicacion = "Edificio C" },
+                new { Nombre = "Laboratorio de Redes", Ubicacion = "Edificio A" }
+            };
+
+            lstBxLaboratorios.ItemsSource = laboratorios;
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (colMenu.Width.Value == 230)
+            {
+                colMenu.Width = new GridLength(0);
+            }
+            else
+            {
+                colMenu.Width = new GridLength(230);
+            }
+        }
+
+        private void btnLaboratorios_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventanaUsuarios = new MainWindow();
+            ventanaUsuarios.Show();
+            this.Close();
+        }
+
+        private void btnUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            UsuariosWindow ventanaUsuarios = new UsuariosWindow();
+            ventanaUsuarios.Show();
+            this.Close();
+        }
+    }
+}
---
+++ SEKTY_ON/UsuariosWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SEKTY_ON
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class UsuariosWindow : Window
+    {
+        public UsuariosWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (colMenu.Width.Value == 230)
+            {
+                colMenu.Width = new GridLength(0);
+            }
+            else
+            {
+                colMenu.Width = new GridLength(230);
+            }
+        }
+
+        private void btnLaboratorios_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventanaUsuarios = new MainWindow();
+            ventanaUsuarios.Show();
+            this.Close();
+        }
+
+        private void btnUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            UsuariosWindow ventanaUsuarios = new UsuariosWindow();
+            ventanaUsuarios.Show();
+            this.Close();
+        }
+    }
+}
